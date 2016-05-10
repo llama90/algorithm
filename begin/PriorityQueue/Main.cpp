0d276879ef7697f8c69e64645d2e92e345f9fb6f
hsseo <hsseo0501@gmail.com>
2016-05-10 19:53:09 +0900

    Imp. Binary Min Heap

--- begin/PriorityQueue/Main.cpp
+++ begin/PriorityQueue/Main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "PriorityQueue.cpp"
+#include "BinaryHeap.cpp"
 using namespace std;
 
 int main() {
 	
 	PriorityQueue * pq = new PriorityQueue();
 
-	int arr[] = {1, 2, 10, 20, 40, 32, 44, 51, 6 };
+	int arr[] = {3, 2, 1, 7, 8, 4, 10, 16, 12 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 
 	cout << "Elements : ";
@@ -15,11 +16,22 @@ int main() {
 	} 
 	cout << endl;
 
-	int KthElement = pq->FindTheKthElement(true, arr, size, 4);
+/*	int KthElement = pq->FindTheKthElement(true, arr, size, 4);
 	cout << "4th smallest element: " << KthElement << endl; 
 	
 	KthElement = pq->FindTheKthElement(false, arr, size, 4);
 	cout << "4th largest element: " << KthElement << endl; 
-	
+*/
+
+	BinaryHeap * bh = new BinaryHeap(size);
+	bh->CreateHeap(arr, size);
+	bh->Display();
+
+	cout << "Extract Min : ";
+	for(int i = 0;i < size; i++) {
+		cout << "[" << bh->ExtractMin() << "]";
+	} 
+	cout << endl;
+
 	return 0;
 }
