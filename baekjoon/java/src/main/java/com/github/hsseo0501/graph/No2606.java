e2a23442da95a2cb62c2ce9a555c1004dc824d23
Hyunseok Seo <hsseo0501@gmail.com>
2018-11-27 18:31:02 +0900

    baekjoon [graph] No.2606 solved

---
+++ baekjoon/java/src/main/java/com/github/hsseo0501/graph/No2606.java
@@ -0,0 +1,56 @@
+package com.github.hsseo0501.graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class No2606 {
+    static int N;
+    static int M;
+
+    static int[] visit;
+    static LinkedList<Integer>[] adjacentList;
+    static Queue<Integer> queue = new LinkedList<Integer>();
+    static int numComputer = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        visit = new int[N + 1];
+        adjacentList = new LinkedList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            visit[i] = 0;
+            adjacentList[i] = new LinkedList<Integer>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int s = scanner.nextInt();
+            int d = scanner.nextInt();
+
+            adjacentList[s].add(d);
+            adjacentList[d].add(s);
+        }
+
+        bfs(1);
+        System.out.println(numComputer);
+    }
+
+    public static void bfs(int start) {
+        visit[start] = 1;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            for (int i = 0; i < adjacentList[v].size(); i++) {
+                int adj = adjacentList[v].get(i);
+                if(visit[adj] == 0) {
+                    visit[adj] = 1;
+                    queue.add(adj);
+                    numComputer++;
+                }
+            }
+        }
+    }
+}
