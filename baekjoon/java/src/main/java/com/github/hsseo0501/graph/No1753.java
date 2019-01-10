4fd9834c7ea406330ef28102d9ed1137cd50a458
Hyunseok Seo <hsseo0501@gmail.com>
2019-01-10 21:26:53 +0900

    baekjoon [graph] No.1753 solved, dijkstra algorithm is implemented using priority queue

--- baekjoon/java/src/main/java/com/github/hsseo0501/graph/No1753.java
+++ baekjoon/java/src/main/java/com/github/hsseo0501/graph/No1753.java
@@ -1,7 +1,8 @@
 package com.github.hsseo0501.graph;
 
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class No1753 {
@@ -9,9 +10,10 @@ public class No1753 {
     static int E;
     static int K;
 
-    static int[] costMap;
+    static int[] costArr;
+    static boolean[] visitArr;
     static LinkedList<Vertex>[] adjacentList;
-    static Queue<Vertex> queue = new LinkedList<Vertex>();
+    static PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,10 +21,13 @@ public class No1753 {
         E = scanner.nextInt();
         K = scanner.nextInt();
 
-        costMap = new int[V + 1];
+        costArr = new int[V + 1];
+        visitArr = new boolean[V + 1];
         adjacentList = new LinkedList[V + 1];
+
+        Arrays.fill(costArr, Integer.MAX_VALUE);
+        costArr[K] = 0;
         for (int i = 1; i <= V; i++) {
-            costMap[i] = Integer.MAX_VALUE;
             adjacentList[i] = new LinkedList<Vertex>();
         }
 
@@ -39,34 +44,36 @@ public class No1753 {
 
     public static void bfs(Vertex start) {
         queue.add(start);
-        costMap[start.getIdx()] = start.getCost();
 
         while (!queue.isEmpty()) {
             Vertex v = queue.poll();
-            int vIdx = v.getIdx();
-            for (int i = 0; i < adjacentList[vIdx].size(); i++) {
-                Vertex adj = adjacentList[vIdx].get(i);
-                int vAdj = adj.getIdx();
-                int cost = costMap[vIdx] + adj.getCost();
-                if (costMap[vAdj] > cost) {
-                    costMap[vAdj] = cost;
+
+            if(visitArr[v.getIdx()]){
+                continue;
+            }
+            visitArr[v.getIdx()] = true;
+
+            int vId = v.getIdx();
+            for(Vertex adj : adjacentList[vId]) {
+                if (visitArr[adj.getIdx()] == false) {
+                    costArr[adj.getIdx()] = Math.min(costArr[adj.getIdx()], costArr[vId] + adj.getCost());
+                    queue.offer(new Vertex(adj.getIdx(), costArr[adj.getIdx()]));
                 }
-                queue.add(adj);
             }
         }
     }
 
     public static void printCost() {
         for (int i = 1; i <= V; i++) {
-            if (costMap[i] == Integer.MAX_VALUE) {
+            if (costArr[i] == Integer.MAX_VALUE) {
                 System.out.println("INF");
             } else {
-                System.out.println(costMap[i]);
+                System.out.println(costArr[i]);
             }
         }
     }
 
-    private static class Vertex {
+    private static class Vertex implements Comparable<Vertex> {
         int idx;
         int cost;
 
@@ -81,6 +88,10 @@ public class No1753 {
 
         public int getCost() {
             return cost;
+        }
+
+        public int compareTo(Vertex o) {
+            return this.cost < o.cost ? -1 : 1;
         }
     }
 
