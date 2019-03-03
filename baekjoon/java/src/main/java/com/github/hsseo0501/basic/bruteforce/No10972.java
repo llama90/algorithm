6c94af26266713d44f05506d03ab4ae7d4143641
Hyunseok Seo <hsseo0501@gmail.com>
2019-03-04 08:44:29 +0900

    baekjoon [basic.bruteforce] No.10972 modified with comment

--- baekjoon/java/src/main/java/com/github/hsseo0501/basic/bruteforce/No10972.java
+++ baekjoon/java/src/main/java/com/github/hsseo0501/basic/bruteforce/No10972.java
@@ -2,6 +2,14 @@ package com.github.hsseo0501.basic.bruteforce;
 
 import java.util.Scanner;
 
+/**
+ * 1. Find largest index i such that array[i − 1] < array[i]. (If no such i exists, then this is already the last permutation.)
+ * 2. Find largest index j such that j ≥ i and array[j] > array[i − 1].
+ * 3. Swap array[j] and array[i − 1].
+ * 4. Reverse the suffix starting at array[i].
+ * URL: https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+ */
+
 public class No10972 {
 
     static int N;
