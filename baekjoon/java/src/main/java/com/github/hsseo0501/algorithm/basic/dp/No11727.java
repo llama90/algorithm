f5dcddb71ca0502b3da28292c1bfcf43c737d9b3
Hyunseok Seo <hsseo0501@gmail.com>
2019-03-12 08:56:53 +0900

    baekjoon [algorithm.basic.dp] No.11727 solved

---
+++ baekjoon/java/src/main/java/com/github/hsseo0501/algorithm/basic/dp/No11727.java
@@ -0,0 +1,23 @@
+package com.github.hsseo0501.algorithm.basic.dp;
+
+import java.util.Scanner;
+
+public class No11727 {
+
+    static int N;
+    static int[] tile = new int[1001];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+
+        tile[0] = 1;
+        tile[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            tile[i] = (tile[i - 1] + tile[i - 2] * 2) % 10007;
+        }
+
+        System.out.println(tile[N]);
+    }
+}
