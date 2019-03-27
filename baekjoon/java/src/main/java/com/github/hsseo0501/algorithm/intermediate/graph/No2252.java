a95469a43e604e74d759e79dc474f8dfe5fd84b9
Hyunseok Seo <hsseo0501@gmail.com>
2019-03-28 07:57:07 +0900

    baekjoon [algorithm.intermediate.graph] No.2252 solved

---
+++ baekjoon/java/src/main/java/com/github/hsseo0501/algorithm/intermediate/graph/No2252.java
@@ -0,0 +1,66 @@
+package com.github.hsseo0501.algorithm.intermediate.graph;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class No2252 {
+
+    static int N, M;
+    static ArrayList<Integer>[] adjacentList;
+    static int inDegree[];
+    static ArrayList<Integer> sortedVertices = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        adjacentList = new ArrayList[N + 1];
+        inDegree = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adjacentList[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int src = scanner.nextInt();
+            int dst = scanner.nextInt();
+            adjacentList[src].add(dst);     // src -> dst 이기 때문에 inDegree는 dst에 해당
+            inDegree[dst]++;
+        }
+
+        topologicalSort();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < sortedVertices.size(); i++) {
+            sb.append(sortedVertices.get(i) + " ");
+        }
+        System.out.println(sb);
+
+    }
+
+    static void topologicalSort() {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            int v = queue.poll();
+            sortedVertices.add(v);
+
+            for (int j = 0; j < adjacentList[v].size(); j++) {
+                int adjV = adjacentList[v].get(j);
+                inDegree[adjV]--;
+
+                if (inDegree[adjV] == 0) {
+                    queue.add(adjV);
+                }
+            }
+        }
+    }
+
+}
