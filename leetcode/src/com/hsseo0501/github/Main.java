0a938491815d028a6d77cc108097ee8be3a82849
Hyunseok Seo <hsseo0501@gmail.com>
2020-01-11 13:38:58 +0900

    Deleted [leetcode] directory to create maven

--- leetcode/src/com/hsseo0501/github/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hsseo0501.github;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello Leetcode!");
-    }
-}
