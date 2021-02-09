e8038129f0a9bc9cc9304e7039790bb11f746f70
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-09 23:06:15 +0900

    Add leetcode No0020 - Time Limit Exceeded

---
+++ leetcode/src/main/java/com/github/lucaseo90/No0028.java
@@ -0,0 +1,34 @@
+package com.github.lucaseo90;
+
+public class No0028 {
+    public int strStr(String haystack, String needle) {
+        int answer = -1;
+
+        if (needle.length() == 0) {
+            answer = 0;
+        } else if (haystack.length() == 0) {
+            answer = -1;
+        } else if (haystack.length() >= needle.length()) {
+            char first = needle.charAt(0);
+            for (int i = 0; i < haystack.length(); i++) {
+                if (haystack.charAt(i) != first) {
+                    continue;
+                }
+
+                int firstIndex = i;
+                boolean isContained = true;
+                for (int j = 0; j < needle.length(); j++) {
+                    if (i + j >= haystack.length() || haystack.charAt(i + j) != needle.charAt(j)) {
+                        isContained = false;
+                    }
+                }
+                if (isContained) {
+                    answer = firstIndex;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
