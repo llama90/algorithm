665fda5a1c971214b08cf3203995bdb595815b64
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-21 19:22:21 +0900

    Modify leetcode No64
      The approach changed from brute-force with pruning to DP
    
      Title: Minimum Path Sum
      Difficulty: Medium
      Acceptance: 56.1 %
      Number of attempts: 4
    
      Time Complexity: O(n * m)
      Space Complexity: O(n * m)
    
      Runtime: 2 ms, faster than 83.71% of Java online submissions
      Memory Usage: 41.4 MB, less than 88.25% of Java online submissions

--- leetcode/src/main/java/com/github/lucaseo90/No0064.java
+++ leetcode/src/main/java/com/github/lucaseo90/No0064.java
@@ -1,40 +1,24 @@
 package com.github.lucaseo90;
 
 public class No0064 {
-    private final int[] dx = new int[]{0, 1};
-    private final int[] dy = new int[]{1, 0};
-    private int minimumPathSum = Integer.MAX_VALUE;
-    private int M, N;
-
     public int minPathSum(int[][] grid) {
-        M = grid.length;
-        N = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] memo = new int[m][n];
 
-        boolean[][] visited = new boolean[M][N];
-        traverse(visited, grid, 0, 0, grid[0][0]);
-        return minimumPathSum;
-    }
-
-    public void traverse(boolean[][] visited, int[][] grid, int x, int y, int pathSum) {
-        if (M - 1 == x && N - 1 == y) {
-            if (pathSum < minimumPathSum) {
-                minimumPathSum = pathSum;
-            }
-            return;
-        }
-
-        for (int i = 0; i < dx.length; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-
-            if (nextX < M && nextY < N && !visited[x][y]) {
-                int currentSum = pathSum + grid[nextX][nextY];
-                if (currentSum < minimumPathSum) {
-                    visited[x][y] = true;
-                    traverse(visited, grid, nextX, nextY, pathSum + grid[nextX][nextY]);
-                    visited[x][y] = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    memo[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    memo[i][j] = grid[i][j] + memo[i][j - 1];
+                } else if (j == 0) {
+                    memo[i][j] = grid[i][j] + memo[i - 1][j];
+                } else {
+                    memo[i][j] = Math.min(grid[i][j] + memo[i - 1][j], grid[i][j] + memo[i][j - 1]);
                 }
             }
         }
+        return memo[m - 1][n - 1];
     }
 }
