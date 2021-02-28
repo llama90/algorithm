6ab91b952b1b51c1d181d4cecf940c6de14d7a29
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-28 17:29:19 +0900

    Solve *leetcode No179 (Medium - Largest Number)
      Title: Largest Number
      Difficulty: Medium
      Acceptance: 30.7 %
      Number of attempts: 2
      - First Approach: O(n!) Broute Force
      - Second Approach: O(nlogn) Sorting using the Comparator
        - compare based on the combination of the two numbers being compared
    
      Time Complexity: O(nlogn) = O(n) + O(nlogn) + O(n)
      - the n is the size of int array
        - O(n) : copy an array from int to string for sorting
        - O(n/2) : sort an string array using the Comparator
        - O(n) : iterate for the size of an array to get summation
      Space Complexity: O(n) = O(n)
      - the n is the size of an int array
        - O(n) : the size of an string array
    
      Runtime: 8 ms, faster than 34.41% of Java online submissions
      Memory Usage: 38.4 MB, less than 68.79% of Java online submissions

--- leetcode/src/main/java/com/github/lucaseo90/No0179.java
+++ leetcode/src/main/java/com/github/lucaseo90/No0179.java
@@ -1,43 +1,31 @@
 package com.github.lucaseo90;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class No0179 {
-    private static ArrayList<Integer> elements = new ArrayList<>();
-    private static String largestNumber = "0";
-
-    public static String largestNumber(int[] nums) {
-        int size = nums.length;
-        boolean[] picked = new boolean[size];
-        largestNumber = "0";
-        findLargestNumber(nums, picked, size, 0);
-        return largestNumber;
+    private static class LargestNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+            return order2.compareTo(order1);
+        }
     }
 
-    public static void findLargestNumber(int[] nums, boolean[] picked, int size, int depth) {
-        if (elements.size() == size) {
-            StringBuilder number = new StringBuilder();
-            for (int i = 0; i < elements.size(); i++) {
-                number.append(elements.get(i));
-            }
-            BigInteger largest = new BigInteger(largestNumber);
-            BigInteger compare = new BigInteger(number.toString());
-            if (largest.compareTo(compare) < 0) {
-                largestNumber = number.toString();
-            }
-
-            return;
-        }
-
-        for (int i = depth; i < size; i++) {
-            if (!picked[i]) {
-                picked[i] = true;
-                elements.add(nums[i]);
-                findLargestNumber(nums, picked, size, depth);
-                elements.remove(elements.size() - 1);
-                picked[i] = false;
+    public static String largestNumber(int[] nums) {
+        StringBuilder largestNumber = new StringBuilder();
+        String[] arr = Arrays.stream(nums).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(arr, new LargestNumberComparator());
+        System.out.println();
+        if (arr[0].equals("0")) {
+            largestNumber.append("0");
+        } else {
+            for (String number : arr) {
+                largestNumber.append(number);
             }
         }
+
+        return largestNumber.toString();
     }
 }
