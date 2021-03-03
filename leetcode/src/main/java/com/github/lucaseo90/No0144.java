8fd17a4ba2d46254041177720f1e88f378477b23
Hyunseok Seo <hsseo0501@gmail.com>
2021-03-03 23:34:46 +0900

    Solve leetcode No144 (Medium - Binary Tree Preorder Traversal)
      Title: Binary Tree Preorder Traversal
      Difficulty: Medium
      Acceptance: 57.5 %
      Number of attempts: 1
    
      Time Complexity: O(n) = O(n)
      - the n is the number of nodes on the tree
        - O(n) : traverse the nodes on the tree
    
      Space Complexity: O(n) = O(n)
      - the n is the number of nodes on the tree
        - O(n) : the number of nodes
    
      Runtime: 0 ms, faster than 100.00% of Java online submissions
      Memory Usage: 37.3 MB, less than 50.65% of Java online submissions

---
+++ leetcode/src/main/java/com/github/lucaseo90/No0144.java
@@ -0,0 +1,30 @@
+package com.github.lucaseo90;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class No0144 {
+
+    private List<Integer> nodes = new LinkedList<Integer>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        preorder(root);
+        return nodes;
+    }
+
+    public void preorder(TreeNode node) {
+        if(node == null) {
+            return;
+        }
+
+        nodes.add(node.val);
+        if(node.left != null) {
+            preorderTraversal(node.left);
+        }
+
+        if(node.right != null) {
+            preorderTraversal(node.right);
+        }
+    }
+
+}
---
+++ leetcode/src/test/java/com/github/lucaseo90/No0144Test.java
@@ -0,0 +1,54 @@
+package com.github.lucaseo90;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class No0144Test {
+
+    private No0144 no0144 = new No0144();
+
+    @Test
+    public void example01() {
+        TreeNode tree =
+                new TreeNode(
+                        1,
+                        null,
+                        new TreeNode(2,
+                                new TreeNode(3),
+                                null)
+                );
+        Assert.assertEquals("[1, 2, 3]", no0144.preorderTraversal(tree).toString());
+    }
+
+    @Test
+    public void example02() {
+        Assert.assertEquals("[]", no0144.preorderTraversal(null).toString());
+    }
+
+    @Test
+    public void example03() {
+        Assert.assertEquals("[1]", no0144.preorderTraversal(new TreeNode(1)).toString());
+    }
+
+    @Test
+    public void example04() {
+        TreeNode tree =
+                new TreeNode(
+                        1,
+                        new TreeNode(2),
+                        null
+                );
+        Assert.assertEquals("[1, 2]", no0144.preorderTraversal(tree).toString());
+    }
+
+    @Test
+    public void example05() {
+        TreeNode tree =
+                new TreeNode(
+                        1,
+                        null,
+                        new TreeNode(2)
+                );
+        Assert.assertEquals("[1, 2]", no0144.preorderTraversal(tree).toString());
+    }
+}
