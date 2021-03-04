34a8863f84fa8bdcab4fa3eed0f72d1971b1f653
Hyunseok Seo <hsseo0501@gmail.com>
2021-03-04 21:06:27 +0900

    Solve leetcode No26 (Easy, array - Remove Duplicates from Sorted Array)
      The top interview questions
    
      Title: Remove Duplicates from Sorted Array
      Difficulty: Easy
      Acceptance: 46.6 %
      Number of attempts: 2 (took 9m 54s)
      - misunderstood
    
      Time Complexity: O(n)
      Space Complexity: with O(1) extra memory

---
+++ leetcode/src/main/java/com/github/lucaseo90/interview/array/No0026_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+package com.github.lucaseo90.interview.array;
+
+public class No0026_RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                count++;
+                nums[count] = nums[i + 1];
+            }
+        }
+        return count + 1;
+    }
+
+}
