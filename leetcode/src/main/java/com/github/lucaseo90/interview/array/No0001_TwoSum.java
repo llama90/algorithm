e086d4d425e78e5b197227d2264b5b6d5dd7f271
Hyunseok Seo <hsseo0501@gmail.com>
2021-03-05 00:11:19 +0900

    Solve leetcode No1 (Easy, array - Two Sum)
      The top interview questions
    
      Title: Two Sum
      Difficulty: Easy
      Acceptance: 46.5 %
      Number of attempts: 1 (took 6m 26s)
    
      Time Complexity: O(n)
      Space Complexity: O(n)
    
      A year ago, I solved the time complexity with O(n^2)

---
+++ leetcode/src/main/java/com/github/lucaseo90/interview/array/No0001_TwoSum.java
@@ -0,0 +1,28 @@
+package com.github.lucaseo90.interview.array;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class No0001_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        List<Integer> twoNumber = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!hashMap.containsKey(target - nums[i])) {
+                hashMap.put(nums[i], i);
+            } else {
+                int index = hashMap.get(target - nums[i]);
+                twoNumber.add(index);
+                twoNumber.add(i);
+                break;
+            }
+        }
+
+        int[] twoNumberArray = new int[2];
+        twoNumberArray[0] = twoNumber.get(0);
+        twoNumberArray[1] = twoNumber.get(1);
+        return twoNumberArray;
+    }
+}
