4661c75b070259aedb27b270605e7a2290835e31
Hyunseok Seo <hsseo0501@gmail.com>
2022-06-01 14:10:55 +0900

    feet(leetcode): #10, study the No0189 and No0977

--- leetcode/src/main/java/com/github/lucaseo90/easy/No0977.java
+++ leetcode/src/main/java/com/github/lucaseo90/easy/No0977.java
@@ -1,14 +1,23 @@
 package com.github.lucaseo90.easy;
 
-import java.util.Arrays;
-
 public class No0977 {
+
     public int[] sortedSquares(int[] nums) {
         int[] square = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            square[i] = nums[i] * nums[i];
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                square[right - left] = nums[left] * nums[left];
+                left += 1;
+            } else {
+                square[right - left] = nums[right] * nums[right];
+                right -= 1;
+            }
         }
-        Arrays.sort(square);
+
         return square;
     }
 }
--- leetcode/src/main/java/com/github/lucaseo90/medium/No0189.java
+++ leetcode/src/main/java/com/github/lucaseo90/medium/No0189.java
@@ -3,16 +3,23 @@ package com.github.lucaseo90.medium;
 public class No0189 {
     public int[] rotate(int[] nums, int k) {
         k = k % nums.length;
-        int[] rotated = new int[nums.length];
-        for (int i = 0; i < k; i++) {
-            rotated[i] = nums[nums.length - k + i];
-        }
-        for (int i = k; i < nums.length; i++) {
-            rotated[i] = nums[i - k];
-        }
-        for (int i = 0;i < nums.length; i++) {
-            nums[i] = rotated[i];
-        }
+        int left = 0;
+        int right = nums.length - 1;
+
+        swap(nums, left, right);
+        swap(nums, 0, k - 1);
+        swap(nums, k, nums.length - 1);
+
         return nums;
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
